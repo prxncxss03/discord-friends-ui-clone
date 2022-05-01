e6cb53dbd5422506c20d08a7ab6f4f6f1e5859a0
Princess Pocon <pocon.evangeline@gmail.com>
2022-05-01 23:16:50 +0800

    sidebar done

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,15 +1,34 @@
-import { BsPlus, BsFillLightningFill, BsGearFill } from 'react-icons/bs';
-import { FaFire, FaPoo } from 'react-icons/fa';
+import { BsPlus} from 'react-icons/bs';
+import {  FaDiscord, FaCompass } from 'react-icons/fa';
+import { MdOutlineFileDownload } from "react-icons/md";
+import arch from '../images/arch.png';
+import dog from '../images/dog.jpeg';
+import perl from '../images/perl.png';
+import spanish from '../images/spanishFlag.png';
 
 export const Sidebar = () => {
     return (
-        <div className="fixed top-0 left-0 h-screen w-16 bg-gray-900 m-0
-        flex flex-col text-white shadow-lg">
+        <div className="fixed top-0 left-0 h-screen bg-gray-900 m-0
+        flex flex-col text-white shadow-lg  w-16">
 
-            <SidebarIcon icon={<FaFire size="30"/>} text={'Fire 🔥'} />
-            <SidebarIcon icon={<FaPoo size="20"/>} text={'Poop 💩'}/>
-            <SidebarIcon icon={<BsPlus size="28" />} text={'Add 📇'}/>
-            <SidebarIcon icon={<BsFillLightningFill size="22" />} text={'Storm ⚡️'} />
+            <SidebarHome icon={<FaDiscord size="30"/>} text={'Home'} />
+            <hr className='sidebar-hr'/>
+            <SidebarIcon icon={
+                <SidebarImg src={dog} alt={"Brown Dog"} classN={"sidebar-full-img"}></SidebarImg>
+            } text={'Dog Lovers'}/>
+            <SidebarIcon icon={
+                <SidebarImg src={arch} alt={"Arch Logo"} classN={"w-7 h-7"}></SidebarImg>
+            } text={'Arch Linux Community 😎 '}/>
+            <SidebarIcon icon={
+                <SidebarImg src={perl} alt={"Camel"} classN={"sidebar-full-img"}></SidebarImg>
+            } text={'Perl Fanatics'}/>
+            <SidebarIcon icon={
+                <SidebarImg src={spanish} alt={"Spanish Flag"} classN={"sidebar-full-img"}></SidebarImg>
+            } text={'Aprender Español 🇪🇸'}/>
+            <SidebarIcon icon={<BsPlus size="32" />} text={'Add a Server'}/>
+            <SidebarIcon icon={<FaCompass size="20" />} text={'Explore Public Servers'}/>
+            <hr className='sidebar-hr'/>
+            <SidebarIcon icon={<MdOutlineFileDownload size="27" />} text={'Download Apps'} />
            
         </div>
     )
@@ -25,4 +44,21 @@ function SidebarIcon ({icon, text}) {
     </div>
     )
 
+}
+
+const SidebarHome = ({icon, text}) => {
+    return (
+        <div className="sidebar-home group">
+        {icon}
+        <span className="sidebar-tooltip group-hover:scale-100">
+            {text}
+        </span>
+    </div>
+    )
+}
+const SidebarImg = ({classN, src, alt}) => {
+    return (
+        <img  className={classN} src={src} alt={alt} />
+    )
+    
 }
