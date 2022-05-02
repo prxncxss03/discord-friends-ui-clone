56c08d4d0ab09f11763658f1d937df93deb73181
Princess Pocon <pocon.evangeline@gmail.com>
2022-05-02 15:26:25 +0800

    almost done in main select

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import { Sidebar } from './components/Sidebar';
-
+import { MainSidebar } from './components/mainSideBar/MainSidebar';
 
 
 function App() {
   return (
-    <div className="App">
-      <Sidebar></Sidebar>
-    
+    <div className="App font-radio font-bold">
+      <Sidebar></Sidebar> 
+      <MainSidebar></MainSidebar>
+
     </div>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,16 +4,29 @@ module.exports = {
   content: [
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
-  
   theme: {
     extend: {
+      spacing : {
+        '1px' : '1px',
+        '10px' : '10px',
+      },
+      fontFamily: {
+        'radio' : ['Radio Canada', 'sans-serif'],
+      },
+      boxShadow : {
+        'defi' : '0 1px 0 rgba(4,4,5,0.2),0 1.5px 0 rgba(6,6,7,0.05),0 2px 0 rgba(4,4,5,0.05)'
+      },
       colors: {
         gray: {
           900: '#202225',
           800: '#2f3136',
           700: '#36393f',
+          650: '#4f545c99',
           600: '#4f545c',
+          450: '#b9bbbe',
+          420: '#96989D',
           400: '#d4d7dc',
+          350: '#a3a6aa',
           300: '#e3e5e8',
           200: '#ebedef',
           100: '#f2f3f5',
