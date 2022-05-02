7cb91661a4663e2ffe582e06e5fcd8b36685f94f
Princess Pocon <pocon.evangeline@gmail.com>
2022-05-02 20:00:22 +0800

    done persons

--- src/App.js
+++ src/App.js
@@ -5,8 +5,8 @@ import { MainSidebar } from './components/mainSideBar/MainSidebar';
 function App() {
   return (
     <div className="App font-radio font-bold">
-      <Sidebar></Sidebar> 
       <MainSidebar></MainSidebar>
+      <Sidebar></Sidebar> 
 
     </div>
   );
--- src/components/mainSideBar/DirectMessages.js
+++ src/components/mainSideBar/DirectMessages.js
@@ -3,7 +3,13 @@ import offline from '../../images/offline.png';
 export const DirectMessages = () => {
     return (
         <div>
-            <DirectMessagesPersons gender="male" alt="male avatar" text="Mario"/>
+            <DirectMessagesPersons gender="male" alt="male avatar" text="iambooo3"/>
+            <DirectMessagesPersons gender="bottts" alt="bottts" text="SummerXX"/>
+            <DirectMessagesPersons gender="initials" alt="avatar" text="sis kaye"/>
+            <DirectMessagesPersons gender="identicon" alt="avatar" text="Ate Princess"/>
+            <DirectMessagesPersons gender="female" alt="female avatar" text="siang"/>
+            <DirectMessagesPersons gender="micah" alt="micah" text="ravenpocon"/>
+            <DirectMessagesPersons gender="avataaars" alt="micah" text="Sweas s0ng3rists"/>
         </div>
     )
 
@@ -12,20 +18,20 @@ export const DirectMessages = () => {
 const DirectMessagesPersons = ({gender, alt, text}) => {
     const avatarName = Math.round(Math.random() * 100);
     return (
-        <button className='hover:bg-gray-650 flex justify-between items-center w-full'>
+        <button className='hover:bg-gray-650 p-2 ease-in-out rounded-md flex justify-between items-center w-full group '>
             <div className='flex justify-center items-center'>
-                <div className="profile relative">
+                <div className="profile relative z-0">
                     <div className="avatar-wrapper w-8 h-8 flex justify-center bg-white rounded-full">
-                        <img className = "w-6" src={`https://avatars.dicebear.com/api/${gender}/${avatarName}.svg`} alt={alt} />
+                        <img className = "w-6 z-0" src={`https://avatars.dicebear.com/api/${gender}/${avatarName}.svg`} alt={alt} />
                     </div>
                     <div className="offline-wrapper absolute top-5 left-5 bg-gray-800 rounded-lg w-4 h-4 flex justify-center items-center">
                         <img className="w-10px h-10px" src={offline} alt="offline symbol" />
                     </div>
                 </div>
-                <span className='ml-3 text-gray-420 text-sm font-semibold'>{text}</span>
+                <span className='ml-3 group-hover:text-gray-400 text-gray-420 text-sm font-semibold'>{text}</span>
             </div>
             <button>
-                <svg  aria-hidden="false" width="16" height="16" viewBox="0 0 24 24"><path fill="#96989D" d="M18.4 4L12 10.4L5.6 4L4 5.6L10.4 12L4 18.4L5.6 20L12 13.6L18.4 20L20 18.4L13.6 12L20 5.6L18.4 4Z"></path></svg>
+                <svg className='opacity-0 group-hover:opacity-100' aria-hidden="false" width="16" height="16" viewBox="0 0 24 24"><path fill="#96989D" d="M18.4 4L12 10.4L5.6 4L4 5.6L10.4 12L4 18.4L5.6 20L12 13.6L18.4 20L20 18.4L13.6 12L20 5.6L18.4 4Z"></path></svg>
             </button>
         </button>
     )
