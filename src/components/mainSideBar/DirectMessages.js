90cc40f8ccf1794a4d9bf51e3726b1f329755687
Princess Pocon <pocon.evangeline@gmail.com>
2022-05-02 20:15:46 +0800

    done direct messages

--- src/components/mainSideBar/DirectMessages.js
+++ src/components/mainSideBar/DirectMessages.js
@@ -2,7 +2,8 @@ import offline from '../../images/offline.png';
 
 export const DirectMessages = () => {
     return (
-        <div>
+        <div className='mx-2'>
+            <DirectMessagesline/>
             <DirectMessagesPersons gender="male" alt="male avatar" text="iambooo3"/>
             <DirectMessagesPersons gender="bottts" alt="bottts" text="SummerXX"/>
             <DirectMessagesPersons gender="initials" alt="avatar" text="sis kaye"/>
@@ -30,9 +31,18 @@ const DirectMessagesPersons = ({gender, alt, text}) => {
                 </div>
                 <span className='ml-3 group-hover:text-gray-400 text-gray-420 text-sm font-semibold'>{text}</span>
             </div>
-            <button>
+            <button className='mr-1'>
                 <svg className='opacity-0 group-hover:opacity-100' aria-hidden="false" width="16" height="16" viewBox="0 0 24 24"><path fill="#96989D" d="M18.4 4L12 10.4L5.6 4L4 5.6L10.4 12L4 18.4L5.6 20L12 13.6L18.4 20L20 18.4L13.6 12L20 5.6L18.4 4Z"></path></svg>
             </button>
         </button>
+    )
+}
+
+const DirectMessagesline = () => {
+    return (
+        <div className='text-gray-420 mx-3 mt-1 flex justify-between items-center'>
+            <span className='text-xs'>DIRECT MESSAGES</span>
+            <button className='text-xl text-gray-400'>+</button>
+        </div>
     )
 }
