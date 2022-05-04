73e04004d3cd37d407a7e0c948b89006a3173177
Princess Pocon <pocon.evangeline@gmail.com>
2022-05-04 08:35:05 +0800

    save the unsaved

--- src/components/navigation/Navigation.js
+++ src/components/navigation/Navigation.js
@@ -3,7 +3,7 @@ import { RightSideNavigation } from "./RightSideNavigation";
 
 export const Navigation = () => {
     return (
-        <div className="bg-gray-700 ">
+        <div className="bg-gray-700 flex items-center ">
             <LeftSideNavigation></LeftSideNavigation>
         </div>
     )
